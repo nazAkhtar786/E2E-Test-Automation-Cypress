193bc32bdd1a9252f402441346c2f8c559d69db6
Naj Akhtar <naazinsh@gmail.com>
2023-07-14 17:06:13 +0530

    updated script

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://www.moneycorp.com/en-us/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/support/PageObject/MoneyCorp.js
+++ cypress/support/PageObject/MoneyCorp.js
@@ -5,8 +5,43 @@ class loginPage{
          languageValidation : () => cy.get('.nav-inner > ul > :nth-child(9) > a'),    
          businessLink : () => cy.get(':nth-child(1) > .hover-trigger > .section-header > .ignoreScrollEvents'),
          helpSupportLink : () => cy.get('.open > .hover-trigger > #mega-nav-1 > .nav-inner-container > .nav-inner >'),
-         ForeighExchangeLink : () => cy.get('.open > :nth-child(3) > a')
+         ForeighExchangeLink : () => cy.get('.open > :nth-child(3) > a'),
+         mouseHoverBusiness : () => cy.get('.c-hamburger__inner'),
+         searchInternationalPayment : () => cy.get('.nav-search-bar > .c-header__search'),
+         SearchButton : () => cy.get('.nav-search-bar > .c-header__search > form > [type="submit"]'),
      }
-  }
+     changeLanguage()
+     {
+         this.elements.languageRegion().click()
+    }
+    verifyLanguageChanged()
+    {
+        this.elements.languageValidation().click()
+    }
+    clickThreeDotIcon()
+    {
+        this.elements.mouseHoverBusiness().click()
+    }
+    clickBusiness()
+    {
+        this.elements.businessLink().click()
+    }
+    clickHelpSupport()
+    {
+        this.elements.helpSupportLink().click()
+    }
+    clickForeignExchange()
+    {
+        this.elements.ForeighExchangeLink().click()
+    }
+    typeInternationalPayment()
+    {
+        this.elements.searchInternationalPayment().type('international payment')
+    }
+    clickSearchButton()
+    {
+        this.elements.SearchButton().click()
+}
+}
   
   export default loginPage;
